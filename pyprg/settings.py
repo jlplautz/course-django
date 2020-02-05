1646de8fd068fb35ca34756b245b01b3431d5fd0
61246880 <jorge.plautz@gmail.com>
2020-02-05 12:38:15 -0300

    Otimizado processo de coleta de arquivos estticos
    close #37

--- pyprg/settings.py
+++ pyprg/settings.py
@@ -145,9 +145,9 @@ if AWS_ACCESS_KEY_ID:
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400', }
     AWS_PRELOAD_METADATA = True
     AWS_AUTO_CREATE_BUCKET = False
-    AWS_QUERYSTRING_AUTH = True
+    AWS_QUERYSTRING_AUTH = False
 
-    AWS_DEFAULT_ACL = 'private'
+    AWS_DEFAULT_ACL = None
 
     # Static Assets
     # ===========================================================
