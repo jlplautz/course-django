8037e73ce117a82697d62d6bca203e1d68df35c6
61246880 <jorge.plautz@gmail.com>
2020-02-04 21:29:47 -0300

    Configurado Lingua e fuso horario
    close #29

--- pyprg/settings.py
+++ pyprg/settings.py
@@ -112,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'America/Sao_Paulo'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'pt-br'
 
 USE_I18N = True
 
