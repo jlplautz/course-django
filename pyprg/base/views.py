a803594b5e80899eb54feb3dfd83f414e9090338
61246880 <jorge.plautz@gmail.com>
2020-02-06 09:07:42 -0300

    Instalado a lib Sentry-sdk
    close #48

--- pyprg/base/views.py
+++ pyprg/base/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
+    # raise ValueError() # <= para simular erro para o Sentry
     return HttpResponse('<html><body>Ola Django</body></html>', content_type='text/html')
