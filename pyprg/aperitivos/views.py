b1f0f1c23bf460b3546a1fee00f2686fdaf36716
61246880 <jorge.plautz@gmail.com>
2020-02-13 01:28:13 -0300

    Criado apresentação de video segundo Slug
    close #68

--- pyprg/aperitivos/views.py
+++ pyprg/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293}
+    videos = {
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293},
+        'instalacao-windows': {'titulo': 'Instalação Wndows', 'vimeo_id': 387145176},
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
