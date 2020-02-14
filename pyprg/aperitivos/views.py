eea71b93cc3f31b7f1500dabf663192b186fd092
61246880 <jorge.plautz@gmail.com>
2020-02-14 07:50:25 -0300

    Criado acesso de atributo no Template
    close #80

--- pyprg/aperitivos/views.py
+++ pyprg/aperitivos/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 
+
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.slug = slug
+        self.titulo = titulo
+        self.vimeo_id = vimeo_id
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 386792293},
-    {'slug': 'instalacao-windows', 'titulo': 'Instalação Windows', 'vimeo_id': 387145176},
+    Video('motivacao', 'Video Aperitivo: Motivação', 386792293),
+    Video('instalacao-windows', 'Instalação Windows', 387145176),
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
