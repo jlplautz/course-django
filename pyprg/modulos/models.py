2a0713c9fe59748d305f4af095e9f34014ab8c46
61246880 <jorge.plautz@gmail.com>
2020-02-22 17:56:54 -0300

    Criado migração de dados em modelo ja existente
    close #124

--- pyprg/modulos/models.py
+++ pyprg/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -7,9 +8,20 @@ class Modulo(OrderedModel):
     # qual o publico alvo
     publico = models.TextField()
     decricao = models.TextField()
+    slug = models.SlugField(unique=True)
 
     class Meta(OrderedModel.Meta):
         pass
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        """
+        retornar utilizando a função reverse do Django depois cria rum link na api de modulos
+        respectiva ao detalhe do modulo.
+        Para identificar o modulos vamos passar aqui para o reverse um campo de slug que vai identificar
+        unicamente o modulo. Vamos colocar aqui respectivo a esta chave um campo de slug do modulo
+        :return:
+        """
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
