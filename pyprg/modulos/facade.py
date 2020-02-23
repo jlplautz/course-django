b2417ffa77c283d3c84ccefacf2e2e935fc07930
61246880 <jorge.plautz@gmail.com>
2020-02-23 08:47:30 -0300

    Construido pagina Detalhes dos Modulos
    close #126

--- pyprg/modulos/facade.py
+++ pyprg/modulos/facade.py
@@ -9,3 +9,13 @@ def listar_modulos_ordenados() -> List[Modulo]:
     :return:
     """
     return list(Modulo.objects.order_by('order').all())
+
+
+def encontrar_modulo(slug: str) -> Modulo:
+    """
+    definir o slug será uma string e que vamos retornar como resultado o Modulo
+    para retornar o modulo apartir do slug vamos colocar Modulo.objects.
+    :param slug:
+    :return: o atributo slug to meu modelo ter-a que ser igual ao atributo slug que esta sendo recebido
+    """
+    return Modulo.objects.get(slug=slug)
--- pyprg/modulos/views.py
+++ pyprg/modulos/views.py
@@ -1,7 +1,18 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
+from pyprg.modulos import facade
 
 
 def detalhe(request, slug):
-    pass
+    """
+    esta função vai renderizar o template modulos/modulo_detalhe.html
+    para apresentar este detalhe precisaremos chegar ao modelo apartir de seu slug passado aqui como paramento
+    vamos passar aqui como contexto objecto que vai ter chave 'modulo' e vamos procurar este modulo apartir de seu slug
+    Para fazer isto vamos criar mais uma função na facade.encontrar_modulos(slug) e vamos receber o modulo
+    :param request:
+    :param slug:
+    :return:
+    """
+    modulo = facade.encontrar_modulo(slug)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, })
