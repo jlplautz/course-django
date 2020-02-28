8007e945d93978816c1ee7e22f9e1942f8505430
61246880 <jorge.plautz@gmail.com>
2020-02-28 19:16:18 -0300

    Revertido migração da tabela intermediaria
    close #176

--- pyprg/turmas/models.py
+++ pyprg/turmas/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -7,4 +6,3 @@ class Turma(models.Model):
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
-    matriculas = models.ManyToManyField(get_user_model())
