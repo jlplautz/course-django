7123f19088e75945caf3cd375eee7ad1b703dd12
61246880 <jorge.plautz@gmail.com>
2020-03-03 07:46:09 -0300

    Criado Modelos Relacionados
    close #194

--- pyprg/turmas/admin.py
+++ pyprg/turmas/admin.py
@@ -4,8 +4,19 @@ from django.contrib import admin
 from pyprg.turmas.models import Turma
 
 
+class MatriculaInline(admin.TabularInline):
+    # para acessae a tabela intermdiaria atraves tdo parametro through
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('-data',)
+
+
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    # indicar para admin quem é lista de inlines que será mostrada
+    inlines = [MatriculaInline]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     # ordernar de forma descrecente
