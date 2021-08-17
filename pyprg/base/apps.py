93217160864e9fbb145dce051245d9d187f8de32
61246880 <jorge.plautz@gmail.com>
2021-08-17 20:47:17 -0300

    up

--- pyprg/base/apps.py
+++ pyprg/base/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ModulosConfig(AppConfig):
+class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pyprg.modulos'
+    name = 'pyprg.base'
